ce1f84b7a58853aa14182e1e68dfea90d848b3d3
Akshay kumar <akshaykumar7273@gmail.com>
2017-08-23 19:47:48 +0530

    Add smartmax cpu governer

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -379,6 +379,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
+extern struct cpufreq_governor cpufreq_gov_smartass;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass)
 #endif
 
 
